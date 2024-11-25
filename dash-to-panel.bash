a3aa47d477cba87f5b260183820c758a80167f0a
MatDplot <mat.dubau@gmail.com>
2024-11-25 19:08:38 +0100

    add conf setup

---
+++ dash-to-panel.bash
@@ -0,0 +1,11 @@
+sudo apt update
+sudo apt install git gnome-shell-extensions gnome-tweaks -y
+git clone https://github.com/home-sweet-gnome/dash-to-panel.git
+cd dash-to-panel
+mkdir -p ~/.local/share/gnome-shell/extensions/dash-to-panel@jderose9.github.com
+cp -r * ~/.local/share/gnome-shell/extensions/dash-to-panel@jderose9.github.com
+gnome-extensions enable dash-to-panel@jderose9.github.com
+
+dconf load /org/gnome/shell/extensions/dash-to-panel/ < conf/dash-to-panel-settings.dconf
+
+dconf load / < conf/gnome-settings-backup.dconf
--- setup.bash
+++ setup.bash
@@ -17,6 +17,7 @@ cd App/
 
 #Tunderbird
 # https://support.mozilla.org/fr/kb/installer-thunderbird-sous-linux
+echo "Thunderbird"
 sudo apt install flatpak
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 sudo flatpak install flathub org.mozilla.Thunderbird
@@ -25,57 +26,71 @@ sudo flatpak install flathub org.mozilla.Thunderbird
 
 #Chrome
 # https://doc.ubuntu-fr.org/google_chrome
+echo "Chrome"
 sudo sh -c 'echo "deb [arch=amd64] https://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
 wget -O- https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo tee /etc/apt/trusted.gpg.d/linux_signing_key.pub
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 78BD65473CB3BD13
 sudo apt-key export D38B4796 | sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/chrome.gpg
+sudo apt update
 sudo apt-get install google-chrome-stable
 
 
 
 #VSCode
+echo "VSCode"
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/packages.microsoft.gpg
 sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt update
 sudo apt install -y code
 
 
 
 #Spotify
 # https://www.spotify.com/fr/download/linux/
+echo "Spotify"
 curl -sS https://download.spotify.com/debian/pubkey_6224F9941A8AA6D1.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt update
 sudo apt-get install spotify-client
 
 
 
 #Zoom
+echo "Zoom"
 wget https://zoom.us/client/latest/zoom_amd64.deb
 sudo dpkg -i zoom_amd64.deb
+sudo apt update
 sudo apt --fix-broken install
 
 
 
 #Zotero
 # https://github.com/retorquere/zotero-deb
+echo "Zotero"
 wget -qO- https://raw.githubusercontent.com/retorquere/zotero-deb/master/install.sh | sudo bash
+sudo apt update
 sudo apt install zotero
 
 
 
 #KeepassXC
 # https://doc.ubuntu-fr.org/keepassxc
+echo "KeepassXC"
 sudo apt install keepassxc
 
 
 
 #Mattermost
+echo "Mattermost"
 curl -fsS -o- https://deb.packages.mattermost.com/setup-repo.sh | sudo bash
+sudo apt update
 sudo apt install mattermost-desktop
 
 
 
 #python
+echo "Python"
 sudo apt install python3 python3-pip
 pip3 install --break-system-packages matplotlib numpy pandas scikit-learn tensorflow keras h5py
 pip3 install --break-system-packages jupyterlab notebook
@@ -84,14 +99,17 @@ sudo apt install hdf5-tools
 
 
 #Root
+echo "Root"
 sudo snap install root-framework
 
 
 
 #LibreOffice
+echo "LibreOffice"
 sudo apt install libreoffice
 
 
 
 #Latex
+echo "Latex"
 sudo apt install texlive-full
